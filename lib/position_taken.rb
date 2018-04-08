e70830231adaa50e4eac550c2463f5acdd7c1474
David Moore <hey@iamdavidmoore.com>
2018-04-08 20:10:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,9 @@ def position_taken?(board, index)
   position_checked = board[index]
   if (position_checked == " ") || (position_checked == "")
     false
+  elsif position_checked == "nil"
+    false
+  elsif (position_checked == "X") || (position_checked == "O")
+    false
   end
 end
